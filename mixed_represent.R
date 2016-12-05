241e08743ff0696ec322bdb4a9645a47d99f67ea
進撃の雯雯老師 <askahu6@gmail.com>
2016-12-05 15:25:46 -0600

    Update mixed_represent.R

--- mixed_represent.R
+++ mixed_represent.R
@@ -2,28 +2,38 @@
 rm(list=ls())
 
 #############################
-#Macro
-
-pca <- function(X,ifscale=T){
-  if(ifscale) {X <- scale(as.matrix(X))}
-  m = nrow(X)
-  n = ncol(X)
-  X = scale(X)
-  Xeigen <- svd(as.matrix(X))
-  value <- (Xeigen$d)^2/m
-  value <- cumsum(value/sum(value))
-  score <- X %*% Xeigen$v
-  mat <- Xeigen$v
-  list(score=score,prop=value,mat=mat)
-}
- ###############
+# Mixed model for Survival approximation
+#############################
 
 setwd('C://Users//WenluluSens//Documents//uthealth//qinchuan')
 raw <- read.csv('raw.csv',row.names=1)
 y <- raw[rowSums(is.na(raw))==0,]
 
+library(nlme)
+x.lme <- lme(time~TS+TP+POLH+KI67+DUT+RRM1+RRM2+RRM2B+
+               age+sex+tumor_grade+tumor_stage,random= ~1|chemo/dead,data=y)
+summary(x.lme)
+plot(y$time);lines(fit <- predict(x.lme),col=2)
+cor(fit,y$time)
+
+#############################
+# Classification Setup
 #############################
 
-library(nlme)
-x.lme <- lme(time~.,random=~1|dead,data=y)
+res <- as.numeric(y$time - fit)
+y2 <- ifelse(res>0,y$chemo,1-y$chemo)
+x2 <- as.matrix(dplyr::select(y,-time,-chemo))
 
+#LDA
+library(MASS)
+y2.lda <- lda(y2 ~ x2)
+table(predict(y2.lda)$class,y2)
+
+#TREE
+library(rpart)
+y2.rpart <- rpart(y2 ~ x2)
+table(predict(y2.rpart)>.5,y2)
+
+#Logistic
+y2.logistic <- glm(y2 ~ x2, family = binomial())
+table(predict(y2.logistic)>0,y2)
